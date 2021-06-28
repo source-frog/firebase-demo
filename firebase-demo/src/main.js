4990fd4ca0e7ad03b08021b68938c5f8f75d4bf5
Simon Strobel <simon@codebrew.de>
2021-06-28 11:44:52 +0200

    [FIREBASE] VIDEO 002: Installation and Setup

--- firebase-demo/src/main.js
+++ firebase-demo/src/main.js
@@ -1,31 +1,6 @@
 import Vue from "vue";
 import App from "./App.vue";
 
-import firebase from "firebase/app";
-import "firebase/firestore";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyDVFoOhFYY1zqDcSrgd05IVbonbcSE0AAk",
-  authDomain: "fire-demo-1338.firebaseapp.com",
-  projectId: "fire-demo-1338",
-  storageBucket: "fire-demo-1338.appspot.com",
-  messagingSenderId: "731841145932",
-  appId: "1:731841145932:web:8d7878440eaa9e54e618e9",
-  measurementId: "G-V7K65JK8W0",
-};
-
-firebase.initializeApp(firebaseConfig);
-
-const db = firebase.firestore();
-const demoCollection = db.collection("demo");
-
-demoCollection
-  .doc("pbLh3aRXvxHzNCtp4QKb")
-  .get()
-  .then((doc) => {
-    console.log(doc.data().hello);
-  });
-
 Vue.config.productionTip = false;
 
 new Vue({
